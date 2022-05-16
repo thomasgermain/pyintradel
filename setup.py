0b6bfac117cf58cd6b0b5a6a4c029d32fae6c86c
Thomas Germain <12560542+thomasgermain@users.noreply.github.com>
2022-05-16 09:09:18 +0200

    correct package (#2)
    correct package

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="pyIntradel",
-    version="0.0.2",
+    version="0.0.3",
     description="Python interface for Intradel",
     long_description_content_type="text/markdown",
     long_description=long_description,
