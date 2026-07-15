33c4d3750a332419e71b6d56f2cd897e54c3203c
Thomas Germain <12560542+thomasgermain@users.noreply.github.com>
2026-07-15 12:48:36 +0200

    fix: Auth via cookie

--- pyintradel/main.py
+++ pyintradel/main.py
@@ -1,3 +1,4 @@
+import argparse
 import asyncio
 import json
 import sys
@@ -7,17 +8,38 @@ import aiohttp
 from pyintradel import api
 
 
-async def _run(login: str, password: str, town: str) -> None:
+async def _run(
+    login: str | None, password: str | None, town: str | None, cookie: str | None
+) -> None:
     async with aiohttp.ClientSession() as sess:
-        json.dump(await api.get_data(sess, login, password, town), sys.stdout, indent=2)
+        data = await api.get_data(sess, login, password, town, cookie=cookie)
+        json.dump(data, sys.stdout, indent=2)
+
+
+def _parse_args(argv: list[str]) -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        prog="pyintradel",
+        description="Usage: pyintradel user pass town OR pyintradel --cookie COOKIE",
+    )
+    parser.add_argument("login", nargs="?", help="Login")
+    parser.add_argument("password", nargs="?", help="Password")
+    parser.add_argument("town", nargs="?", help="Town, see pyintradel/api/towns.py")
+    parser.add_argument(
+        "--cookie",
+        help="Session cookie captured from an already logged-in browser, "
+        "used instead of login/password/town",
+    )
+    return parser.parse_args(argv)
 
 
 def main() -> None:
-    if len(sys.argv) != 4:
-        print("Usage: pyintradel user pass town")
-        sys.exit(0)
+    args = _parse_args(sys.argv[1:])
 
-    asyncio.run(_run(sys.argv[1], sys.argv[2], sys.argv[3]))
+    if not args.cookie and not (args.login and args.password and args.town):
+        print("Usage: pyintradel user pass town OR pyintradel --cookie COOKIE")
+        sys.exit(1)
+
+    asyncio.run(_run(args.login, args.password, args.town, args.cookie))
 
 
 if __name__ == "__main__":
